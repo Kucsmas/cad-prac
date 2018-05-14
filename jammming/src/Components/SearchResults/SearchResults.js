7b0ab2d55f7d99c5bce4aee859325aaabca0cdbc
Kucsmas <37742348+Kucsmas@users.noreply.github.com>
2018-05-14 13:03:18 +0100

    Update SearchResults.js

--- jammming/src/Components/SearchResults/SearchResults.js
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -1,14 +1,30 @@
 import React from 'react';
+
 import TrackList from '../TrackList/TrackList.js';
+//import Playlist from '../Playlist/Playlist.js';
+//import Track  from '../Track/Track.js';
+//import SearchBar from '../SearchBar/SearchBar.js';
+
+
 
 import './SearchResults.css';
 
+
+
 class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
-        <TrackList tracks={this.props.searchResults} />
+        <TrackList tracks={this.props.searchResults}
+                   onAdd={this.props.onAdd}
+                   isRemoval={false}/>
       </div>
     );
   }
