d0e2129a7e6fcb65e9d0f3d925af3334cd81bf31
Kucsmas <37742348+Kucsmas@users.noreply.github.com>
2018-05-15 01:23:33 +0100

    Update Spotify.js

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -3,7 +3,34 @@ let expiresIn=0;
 const clientId = 'bf1ca28115634816a65dc097d7ba9356';
 const uri = "http://localhost:3000/";
 
-
+/*search(term) {
+  return fetch(
+    `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }
+        });
+      }
+   });
+}*/
 const Spotify = {
 
   search(term) {
@@ -11,7 +38,7 @@ const Spotify = {
     return fetch(
       `https://api.spotify.com/v1/search?type=track&q=${term}`,
       {
-        headers: {Authorization:'Bearer ${accessToken}'}
+        headers: {Authorization:`Bearer ${accessToken}`}
       }
     ).then(response => {
       return response.json();
@@ -42,79 +69,50 @@ const Spotify = {
         window.setTimeout(() => accessToken = '', expiresIn * 1000);
         window.history.pushState('Access Token', null, '/');
     } else if (!accessToken && window.location.href.match(/access_token=([^&]*)/) === null) {
-      window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${uri}`
+      window.location = `https://accounts.spotify.com/authorize?
+                        client_id=${clientId}
+                        &response_type=token
+                        &scope=playlist-modify-public
+                        &redirect_uri=${uri}`
     }
   },
 
- savePlaylist (name, URIs) {
-    if (!this.tracks.name && !this.tracks.URI ) {
+  savePlaylist (name, URIs) {
+    if (!name && !URIs ) {
       return;
     }
+    const accessToken = Spotify.getAccessToken();
+    const headers = {'Authorization': `Bearer ${accessToken}` };
+    let userId;
 
-    let accessToken = this.getAccessToken();
-    let headers = {
-      Authorization: `https://accounts.spotify.com/authorize?
-                          client_id=${clientId}
-                          &redirect_uri=${uri}
-                          &response_type=token` };
-    let userId = '';
+    return fetch(
+        'https://api.spotify.com/v1/me',
+        {headers: headers}
+      ).then(response => response.json()
+      ).then(jsonResponse => {
+        userId = jsonResponse.id;
+        return fetch(
+          `https://api.spotify.com/v1/users/${userId}/playlists`,
+          {
+            headers: headers,
+            method: 'POST',
+            body: JSON.stringify({name:name})
+          }
+        )
+      }).then(response => response.json()
+    ).then(jsonResponse => {
+      let playlistID = jsonResponse;
+      return playlistID;
+    });
+  }, //savePlaylist
 
-    fetch(
-      'https://api.spotify.com/v1/me',
-      {headers: headers}
-    ).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      userId = jsonResponse.id;
-    }).post(
-      `https://api.spotify.com/v1/users/${userId}/playlists`,
-      {
-        headers: headers,
-        method: this.search(),
-        body: 
-      }
-    ); //post
-  } //savePlaylist
+
+
+
+
+
 
 } //spotify
-
-
-
-
-
-
-export default Spotify;
-
-
-/*search(term) {
-  return fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-    {
-      headers: {
-        Authorization: `Bearer ${apiKey}`
-      }
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      if (jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => {
-          return {
-            id: business.id,
-            imageSrc: business.url,
-            name: business.name,
-            address: business.location.address1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories[0].title,
-            rating: business.rating,
-            reviewCount: business.review_count,
-          }
-        });
-      }
-   });
-}*/
-
 /*var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var regexp = /[A-E]/gi;
 var matches_array = str.match(regexp);
@@ -127,5 +125,4 @@ console.log(matches_array);
 
 
 
-
-
+export default Spotify;
